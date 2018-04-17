d61b85abd92d88bf42267a1923590bc46cccbb6f
Anton Myronyuk <antonmyronyuk@gmail.com>
2018-04-17 22:07:44 +0300

    some rafactoring

--- main.py
+++ main.py
@@ -10,12 +10,11 @@ def file_base_name(file_name):
 
 def parts_sizes(file_size, parts_num):
     part = file_size // parts_num
-    return [part for i in range(parts_num - 1)]\
+    return [part for _ in range(parts_num - 1)]\
         + [file_size - (part * (parts_num - 1))]  # last size
 
 
 def split_file(file_name: str, parts_num: int):
-
     # check parts_num
     try:
         parts_num = int(parts_num)
@@ -28,7 +27,7 @@ def split_file(file_name: str, parts_num: int):
     path = os.getcwd()  # current path
     file_base = file_base_name(file_name)
 
-    # check file
+    # check file size (if it exists)
     try:
         file_size = os.stat(path + '/' + file_name).st_size
     except:
@@ -40,29 +39,23 @@ def split_file(file_name: str, parts_num: int):
 
     # make directory for split files
     try:
-        # os.rmdir(path + '/' + dir_name)
         # remove non empty dir
         shutil.rmtree(path + '/' + dir_name)
-        os.mkdir(path + '/' + dir_name)
         print('directory "' + dir_name + '" was replaced')
     except:
-        os.mkdir(path + '/' + dir_name)
         print('directory "' + dir_name + '" was created')
+    finally:
+        os.mkdir(path + '/' + dir_name)
 
-    '''
-    print('size: ' + str(file_size))
-    print(parts_sizes(1345, 7))
-    print(sum(parts_sizes(1345, 7)))
-    '''
     parts = parts_sizes(file_size, parts_num)
     with open(file_name, 'rb') as f:
-        for i in range(parts_num):
-            chunk = f.read(parts[i])
-            cur_file_path = path + '/' + dir_name + '/' + file_base + str(i) + split_file_ext
+        for ind, part_size in enumerate(parts, 1):
+            cur_file_path = path + '/' + dir_name + '/' + file_base + str(ind) + split_file_ext
             output_file = open(cur_file_path, "wb")
-            output_file.write(chunk)
+            # read and write
+            output_file.write(f.read(part_size))
             output_file.close()
-            print('Part {} - ready!'.format(i + 1))
+            print('Part {} - ready!'.format(ind))
 
     print('That\'s all!')
     print('Check "' + dir_name + '" folder')
@@ -71,15 +64,15 @@ def split_file(file_name: str, parts_num: int):
 def merge_parts(dir_name: str):
     output_name = 'merged_' + re.sub('_split$', '', dir_name)
     output_file = open(output_name, 'wb')
-    print('output file: ' + output_name)
     path = os.getcwd()  # current path
     dir_name = path + '/' + dir_name
-    for ind, file in enumerate(os.listdir(dir_name), 1):
-        with open(dir_name + '/' + file, 'rb') as f:
+    for ind, file_name in enumerate(os.listdir(dir_name), 1):
+        with open(dir_name + '/' + file_name, 'rb') as f:
             output_file.write(f.read())
             print('Part {} - merged!'.format(ind))
     output_file.close()
     print('That\'s all!')
+    print('output file: ' + output_name)
 
 
 if __name__ == '__main__':
